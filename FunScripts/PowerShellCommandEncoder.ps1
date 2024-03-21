750637107b8bdb1164107ba29f97e632ff4e3e77
kylercondran@gmail.com <kylercondran@gmail.com>
2024-03-21 14:46:32 -0300

    added PowerShellCommandEncoder.ps1

---
+++ FunScripts/PowerShellCommandEncoder.ps1
@@ -0,0 +1,7 @@
+$command = 'write-host "LOL"'
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+$scriptStart = "powershell.exe -encodedCommand " 
+$encodedCommand = [Convert]::ToBase64String($bytes)
+$encodedCommand = $scriptStart + $encodedCommand
+$encodedCommand | set-content ("C:\FilePath\Output.ps1")
+#powershell.exe -encodedCommand dwByAGkAdABlAC0AaABvAHMAdAAgACIATABPAEwAIgA=
