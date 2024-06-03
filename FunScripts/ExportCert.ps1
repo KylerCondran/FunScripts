ebfea7174ade1fbd6bce4de5f7d7df4de2e07236
kylercondran@gmail.com <kylercondran@gmail.com>
2024-06-03 14:20:23 -0300

    added exportcert.ps1

---
+++ FunScripts/ExportCert.ps1
@@ -0,0 +1,9 @@
+#Uses a certificate from the microsoft certificate store, replace the zeros with the certificate identifier.
+$cert = get-item -path cert:\currentuser\my\0000000000000000000000000000000000000000 -ea stop
+#$password = ""
+$type = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
+$bytes = $cert.Export($type)
+[System.IO.File]::WriteAllBytes("FileName.cer", $bytes)
+#$error | Out-File "C:\FilePath\Errors.txt"
+write-host "Done."
+start-sleep 120
