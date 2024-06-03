47b66f329fb361d1bb8854512f383dfe55b56c23
kylercondran@gmail.com <kylercondran@gmail.com>
2024-06-03 14:32:06 -0300

    added getcertbyname.ps1

---
+++ FunScripts/GetCertByName.ps1
@@ -0,0 +1,21 @@
+$validCerts = @(
+    Get-ChildItem -Path Cert:\CurrentUser\My |
+    Where-Object {
+        $_.PrivateKey -is [System.Security.Cryptography.RSACryptoServiceProvider] -and
+        $_.Issuer -eq "CN=CERTNAME"
+    }
+)
+if ($validCerts.Count -eq 0)
+{
+    write-host "No RSA certificates with usable private keys were found in the current user's store."
+	start-sleep 120
+}
+if ($validCerts.Count -eq 1)
+{
+    write-host "Certificate Found."
+	$cert = $validCerts[0]
+	$type = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
+	$bytes = $cert.Export($type)
+	[System.IO.File]::WriteAllBytes("C:\FilePath\FileName.cer", $bytes)
+	start-sleep 120
+}
